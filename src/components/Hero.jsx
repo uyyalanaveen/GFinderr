81a1d4894875871b208a144f5a57b668327ddf46
Gopi Krishna <157501368+GopiKrishna3581@users.noreply.github.com>
2024-09-10 20:07:44 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import UserSearch from "./search-input.jsx";
 
 export default function Hero() {
     return (
-        <div className="lg:m-12 m-6 mx-8 h-[70vh] border border-gray-500 py-5  rounded-2xl">
+        <div className="lg:m-12 m-6 mx-8 h-[70vh] border border-gray-500 py-6  rounded-2xl">
             <div className="flex mx-9 items-center justify-center">
                 <UserSearch />
             </div>
